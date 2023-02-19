74a8d4a58da0fdcc0c5bcfd9eb353f6e0b1947d8
DELL <DELL@192.168.1.4>
2023-02-19 13:40:26 +0530

    First change

---
+++ seleniumpr1/src/getmethod/Selenium.java
@@ -0,0 +1,20 @@
+package getmethod;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Selenium {
+
+	public static void main(String[] args) {
+		
+		System.setProperty("webdriver.chrome.driver","C:\\Selenium\\chromedriver_win32\\chromedriver.exe");
+		
+		WebDriver driver= new ChromeDriver ();
+		
+		driver.get ("https://www.facebook.com/");
+		
+		
+
+	}
+
+}
